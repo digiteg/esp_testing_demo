0cfdfa2c0f11310ceb164ad448fefc8303590462
Milan Varga <mvarga@gmail.com>
2020-07-08 02:34:34 +0200

    Create fixtures.py

---
+++ src2/fixtures.py
@@ -0,0 +1,5 @@
+# Test fixture: test fixture is the fixed state of the objects used as the source when performing tests.
+# The purpose of using fixture is if you have a complex test case, then preparing the desired state can easily take a lot of resources 
+# (for example, you consider a function with certain accuracy and each next sign of accuracy in the calculations takes a day). 
+# Using fixture (on slang - fixtures), we skip the preliminary preparation of the state and immediately begin testing. 
+# Test fixture can appear, for example, in the form (database state, set of environment variables, set of files with the necessary content).
