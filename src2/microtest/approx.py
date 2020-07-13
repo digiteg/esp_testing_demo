57bce5567ae544f1f0aa1032d92466ad9d49c048
Milan Varga <mvarga@gmail.com>
2020-07-14 01:55:56 +0200

    approx

--- src2/microtest/approx.py
+++ src2/microtest/approx.py
@@ -2,11 +2,11 @@ DEFAULT_ABSOLUTE_TOLERANCE = 1e-12  # type: Union[float, Decimal]
 DEFAULT_RELATIVE_TOLERANCE = 1e-6  # type: Union[float, Decimal]
 
 
-def set_default(x, default):
-    return x if x is not None else default
+def approx(actual, expected, abst=None, relt=None):
 
+    def set_default(x, default):
+        return x if x is not None else default
 
-def approx(actual, expected, abst, relt):
     absolute_tolerance = set_default(abst, DEFAULT_ABSOLUTE_TOLERANCE)
     relative_tolerance = set_default(
         relt, DEFAULT_RELATIVE_TOLERANCE) * abs(expected)
@@ -15,5 +15,26 @@ def approx(actual, expected, abst, relt):
     tolval = max(relative_tolerance, absolute_tolerance)
     return abs(expected - actual) <= tolval
 
+ #  Boundary Values technique
 
 
+def test_approx0():
+    assert approx(0, 0, 0, 0) == True  # test edge 0
+
+
+def test_approx_edge1():
+    assert approx(0, 1, 1, 1) == True   # test edge 1
+
+
+def test_fib_internal21():
+    assert approx(2, 1, 3, 3) == True  # test internal point expected 21
+
+
+def test_fib_internalFloat():
+    assert approx(3.1934, 3.2, 0.01) == True  # test internal point expected 21
+
+
+test_approx0()
+test_approx_edge1()
+test_fib_internal21()
+test_fib_internalFloat()
